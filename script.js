bf8895db4f06f2d0e814c269316bca161f446413
Aditya Mohan <adi.mohan1999@gmail.com>
2021-08-01 03:24:10 +0530

    lqast push

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
+const file = document.getElementById('fileupload');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
@@ -22,21 +23,51 @@ container.addEventListener('click',function(){
     const barWidth = canvas.width/bufferLength;
     let barHeight;
     let x;
-
+    
     function animate(){
         x = 0;
         ctx.clearRect(0,0,canvas.width,canvas.height);
         analyser.getByteFrequencyData(dataArray);
         for(let i = 0; i<bufferLength;i++){
-            barHeight = dataArray[i];
-            ctx.fillStyle = 'white';
+            barHeight = dataArray[i]*2;
+            ctx.fillStyle = 'red';
             ctx.fillRect(x,canvas.height - barHeight,barWidth,barHeight)
             x+=barWidth
         }
         requestAnimationFrame(animate);
     }
     animate();
-
+    
     
 })
 
+file.addEventListener('change',function(){
+    const files = this.files
+    const audio1 = document.getElementById('audio1');
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load()
+    audio1.play()
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 64;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    
+    const barWidth = canvas.width/bufferLength;
+    let barHeight;
+    let x;
+    function animate(){
+        x = 0;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        for(let i = 0; i<bufferLength;i++){
+            barHeight = dataArray[i]*2;
+            ctx.fillStyle = 'red';
+            ctx.fillRect(x,canvas.height - barHeight,barWidth,barHeight)
+            x+=barWidth
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
+    
+})
