4a966b0cafd958d281adce4905c4ecaa1ada4ab6
Aditya Mohan <adi.mohan1999@gmail.com>
2021-07-31 09:46:31 +0530

    Commit with audio added

---
+++ script.js
@@ -0,0 +1,6 @@
+const button1 = document.getElementById('button1');
+let audio1 = new Audio();
+audio1.src = "./assets/audio/spell.wav";
+button1.addEventListener('click',function(){
+    audio1.play()
+})
